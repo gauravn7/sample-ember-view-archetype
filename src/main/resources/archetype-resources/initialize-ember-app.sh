7d500f75668a447a0c54e3318c70961edb8e1959
Gaurav Nagar <gnagar@hortonworks.com>
2016-06-24 01:41:21 +0530

    initial commit

---
+++ src/main/resources/archetype-resources/initialize-ember-app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+unzip src/main/resources/ui.zip -d src/main/resources
+rm -rf src/main/resources/ui.zip
---
+++ src/main/resources/archetype-resources/src/main/java/HelpService.java
@@ -0,0 +1,55 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ${package};
+
+import org.apache.ambari.view.ViewContext;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+
+/**
+ * Help service
+ */
+public class HelpService {
+
+  @Inject
+  ViewContext context;
+
+  /**
+   * Username
+   * @return username
+   */
+  @GET
+  @Path("/username")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response username() {
+    HashMap<String, Object> response = new HashMap<String, Object>();
+    response.put("username", context.getUsername());
+    return Response.ok(response).build();
+  }
+
+}
